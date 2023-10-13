1e7e30254617effa4acecac4cfc258c9b21b838d
Dylanson25 <dvillarreal0@ucol.mx>
2023-10-12 21:33:52 -0600

    auth  hadle message imlemented

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,21 +1,19 @@
 const { authenticate } = require('../services/auth');
 const AuthException = require('../exceptions/auth');
+const { ERR_AUTH } = require('../hooks/codeStatus');
+const { handleResponse } = require('../responses');
 
 exports.login = async function (request, response) {
   const { email, password } = request.body;
+  const _response = new handleResponse('succes', {}, 200, 'inicio de sesion exitoso');
 
   try {
-    const token = await authenticate({email, password});
-    response.status(200).json({
-      jwt: token,
-    });
-  } catch(e) {
-    if(e instanceof AuthException){
-      return response.status(400).json({
-        message: 'Email o contraseña inválidos',
-        messagedev: 'No se encontro el usuario en la base de datos',
-        code: 'ERR_AUTH',
-      });
+    const token = await authenticate({ email, password });
+    _response.data = { jwt: token };
+    response.status(200).json(_response.get());
+  } catch (e) {
+    if (e instanceof AuthException) {
+      return response.status(400).json(_response.getByErrorType(ERR_AUTH));
     }
     throw e;
   }
