f63f120579684b61ac104cfa9465fb70a19f69fc
Dylanson25 <dvillarreal0@ucol.mx>
2023-10-12 21:34:58 -0600

    code status hook

---
+++ src/hooks/codeStatus.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-undef */
+
+exports.ERR_EMAIL = Symbol('ERR_EMAIL');
+exports.ERR_PHONE_NUMBER = Symbol('ERR_PHONE_NUMBER');
+exports.ERR_AUTH = Symbol('ERR_AUTH');
+exports.ERR_UNKNOWN = Symbol('ERR_UNKNOWN');
+exports.ERR_RESOURCE_NOT_FOUND = Symbol('ERR_RESOURCE_NOT_FOUND');
