fa3ca5359866250167e76a27e5ab3e334fa9a896
Dylanson25 <dvillarreal0@ucol.mx>
2023-10-12 21:42:17 -0600

    implement respnse handler in midlewares

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 //To use env variables
-require("dotenv").config();
+require('dotenv').config();
 
 //Imports
 const userRouter = require('./routers/user');
-const authRouter = require("./routers/auth");
-const validationError = require("./middlewares/validation-error");
-const unknownError = require("./middlewares/unknown-error");
+const authRouter = require('./routers/auth');
+const validationError = require('./middlewares/validation-error');
+const unknownError = require('./middlewares/unknown-error');
 const employerRouter = require('./routers/employer');
 
 //DB configuration
-const { initDatabase } = require("./db");
+const { initDatabase } = require('./db');
 initDatabase();
 
-const express = require("express");
+const express = require('express');
 const app = express();
 
 app.use(express.json());
@@ -27,5 +27,5 @@ app.use(validationError);
 app.use(unknownError);
 
 app.listen(process.env.SERVER_PORT, function () {
-	console.log("Escuchando puerto " + process.env.SERVER_PORT);
+  console.log('Escuchando puerto ' + process.env.SERVER_PORT);
 });
